57a3991cad735317037e3d01d82f3e14b4ad2811
MilenRusev <milen.rusev21@gmail.com>
2018-01-26 09:20:08 +0200

    Uploads

--- 01.Reverse-String/Properties/AssemblyInfo.cs
+++ 01.Reverse-String/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("2.Simple-Calculator")]
+[assembly: AssemblyTitle("01.Reverse-String")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
