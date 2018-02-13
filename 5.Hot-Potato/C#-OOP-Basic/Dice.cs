0edb6c17dd9de5585b7f8a4ed5a5985b5ea3932b
MilenRusev <milen.rusev21@gmail.com>
2018-02-13 16:27:22 +0200

    updade

---
+++ 5.Hot-Potato/C#-OOP-Basic/Dice.cs
@@ -0,0 +1,5 @@
+namespace 5. Hot_Potato.C# _OOP_Basic {
+    public class Dice {
+
+    }
+}
--- 5.Hot-Potato/Program.cs
+++ 5.Hot-Potato/Program.cs
@@ -1,24 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace HotPotato
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            string[] children = Console.ReadLine().Split();
-            var tossLimit = int.Parse(Console.ReadLine());
-            var queue = new Queue<string>(children);
-            while (queue.Count > 1)
-            {
-                for (int tossCounter = 1; tossCounter < tossLimit; tossCounter++)
-                {
-                    queue.Enqueue(queue.Dequeue());
+namespace HotPotato {
+    class MainClass {
+        public static void Main (string[] args) {
+            string[] children = Console.ReadLine ().Split ();
+            var tossLimit = int.Parse (Console.ReadLine ());
+            var queue = new Queue<string> (children);
+            while (queue.Count > 1) {
+                for (int tossCounter = 1; tossCounter < tossLimit; tossCounter++) {
+                    queue.Enqueue (queue.Dequeue ());
                 }
-                Console.WriteLine($"Removed {queue.Dequeue()}");
+                Console.WriteLine ($"Removed {queue.Dequeue()}");
             }
-            Console.WriteLine($"Last is {queue.Dequeue()}");
+            Console.WriteLine ($"Last is {queue.Dequeue()}");
         }
     }
 }
